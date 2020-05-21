8d2b7bb24c2e45246b6de9a2db1228644baa479a
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-21 20:24:44 +0530

    Connecting to WiFi, receiving incoming WS commands
    network hexdump is showing all msg contents :)

--- src/main.c
+++ src/main.c
@@ -1,39 +1,40 @@
-/*
- * Copyright (c) 2014-2018 Cesanta Software Limited
- * All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the ""License"");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an ""AS IS"" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "mgos.h"
+#include "mgos_wifi.h"
+#include "mgos_rpc.h"
 
-static void timer_cb(void *arg) {
-  static bool s_tick_tock = false;
-  LOG(LL_INFO,
-      ("%s uptime: %.2lf, RAM: %lu, %lu free", (s_tick_tock ? "Tick" : "Tock"),
-       mgos_uptime(), (unsigned long) mgos_get_heap_size(),
-       (unsigned long) mgos_get_free_heap_size()));
-  s_tick_tock = !s_tick_tock;
-#ifdef LED_PIN
-  mgos_gpio_toggle(LED_PIN);
-#endif
-  (void) arg;
+static void sum_cb(struct mg_rpc_request_info *ri, void *cb_arg,
+                   struct mg_rpc_frame_info *fi, struct mg_str args)
+{
+    LOG(LL_INFO, ("executing timer code"));
+    double a = 0, b = 0;
+    if (json_scanf(args.p, args.len, ri->args_fmt, &a, &b) == 2)
+    {
+        mg_rpc_send_responsef(ri, "%.2lf", a + b);
+    }
+    else
+    {
+        mg_rpc_send_errorf(ri, -1, "Bad request. Expected: {\"a\":N1,\"b\":N2}");
+    }
+    (void)cb_arg;
+    (void)fi;
 }
 
-enum mgos_app_init_result mgos_app_init(void) {
-#ifdef LED_PIN
-  mgos_gpio_setup_output(LED_PIN, 0);
-#endif
-  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
-  return MGOS_APP_INIT_SUCCESS;
+// static void my_timer_cb(void *arg)
+// {
+//     LOG(LL_INFO, ("Inside timer function."));
+//     struct mg_rpc_call_opts opts = {.dst = mg_mk_str("ws://192.168.0.104")};
+//     mg_rpc_callf(mgos_rpc_get_global(), mg_mk_str("ping"), NULL, NULL, &opts,
+//                  NULL, NULL, NULL);
+//     (void)arg;
+// }
+
+enum mgos_app_init_result mgos_app_init(void)
+{
+    // Register JSON RPC Callback
+    mg_rpc_add_handler(mgos_rpc_get_global(), "Sum", "{a: %lf, b: %lf}", sum_cb, NULL);
+
+    // Register callback for a timer to send information to the server
+    // mgos_set_timer(5000, MGOS_TIMER_REPEAT, my_timer_cb, NULL);
+
+    return MGOS_APP_INIT_SUCCESS;
 }
